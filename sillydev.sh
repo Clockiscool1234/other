abb0ef5d523c565a7cf00e0cd22cca9e1a6ae865
Clockiscool1234 <85726365+Clockiscool1234@users.noreply.github.com>
2024-10-22 20:22:37 +0800

    Update sillydev.sh

--- sillydev.sh
+++ sillydev.sh
@@ -6,6 +6,7 @@ chmod +x ./proot
 mkdir root
 tar -xf focal-base-amd64.tar.gz -C root
 rm focal-base-amd64.tar.gz
-echo ./proot -0 -b /etc/host.conf -b /etc/hosts -b /etc/nsswitch.conf -b /etc/resolv.conf -b /dev/ -b /sys/ -b /proc/ -b /tmp/ -w /root -r root /usr/bin/env -i HOME=/root USER=root /bin/su > start.sh
-chmod +x start.sh
-echo Done! Run ./start.sh
+echo ./proot -0 -b /etc/host.conf -b /etc/hosts -b /etc/nsswitch.conf -b /etc/resolv.conf -b /dev/ -b /sys/ -b /proc/ -b /tmp/ -w /root -r root /usr/bin/env -i HOME=/root USER=root /bin/su > .bashrc
+echo exit >> .bashrc
+chmod +x .bashrc
+echo Done! Restart to finish.
